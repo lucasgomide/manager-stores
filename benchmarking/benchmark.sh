8c302a6ac7971e7cba776da27c1eeb55beb85cdf
lucasgomide <lucaslg200@gmail.com>
2018-08-06 13:12:55 -0300

    Updating benchmarking script

--- benchmarking/benchmark.sh
+++ benchmarking/benchmark.sh
@@ -1,2 +1,3 @@
-wrk -t3 -c100 -d10s --latency -s ./post.lua  https://manager-stores.herokuapp.com/graphql
+DIR=`pwd`
+wrk -t3 -c100 -d10s --latency -s $DIR/benchmarking/post.lua  https://manager-stores.herokuapp.com/graphql
 
